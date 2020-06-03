ae4aaea52d5be5d412bf0a48b8c1f08f899bff67
Danium <Kividanya964@gmail.com>
2020-06-03 18:15:26 +0300

    posts-/postPage containers is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
+import PostsPageContainer from "./components/PostsPage/PostsPageContainer";
+import PostPageContainer from "./components/PostsPage/PostPageContainer";
 
 function App() {
   return (
     <div>
-
+    <Route path={'/posts'} render={() => <PostsPageContainer/>}/>
+    <Route path={'/post/:postId'} render={() => <PostPageContainer/>}/>
     </div>
   );
 }
