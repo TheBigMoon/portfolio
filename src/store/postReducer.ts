542e44e1681b78003f31c6230caf46eac1d394a6
Danium <Kividanya964@gmail.com>
2020-06-03 17:24:19 +0300

    the first actions is added

---
+++ src/store/postReducer.ts
@@ -0,0 +1,40 @@
+import {ActionType, GetPostType, GetPostsType, SetPostType, SetPostsType, PostType, StateType} from "../types/types";
+
+export const GET_POSTS = 'GET_POSTS';
+export const SET_POSTS = 'SET_POSTS';
+export const GET_POST = 'GET_POST';
+export const SET_POST = 'SET_POST';
+export const UPDATE_POST = 'UPDATE_POST';
+export const SET_UPDATED_POST = 'SET_UPDATED_POST';
+export const DELETE_POST = 'DELETE_POST';
+export const CLEAR_DELETED_POST = 'CLEAR_DELETED_POST';
+
+const initialState: StateType = {
+  posts: null,
+  post: null
+}
+
+export const postReducer = (state: StateType = initialState, action: ActionType) => {
+  switch (action.type) {
+    case SET_POSTS: {
+      return {
+        ...state,
+        posts: action.posts
+      }
+    }
+    case SET_POST: {
+      return {
+        ...state,
+        posts: action.post
+      }
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export const getPosts = (): GetPostsType => ({type: GET_POSTS})
+export const setPosts = (posts: Array<PostType>): SetPostsType => ({type: SET_POSTS, posts})
+export const getPost = (): GetPostType => ({type: GET_POST})
+export const setPost = (post: PostType): SetPostType => ({type: SET_POST, post})
