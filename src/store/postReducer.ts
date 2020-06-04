5f7986d17cb8c8e4ab39d17bd2f3ce80cc641d19
Danium <Kividanya964@gmail.com>
2020-06-04 16:56:49 +0300

    all actions is added

--- src/store/postReducer.ts
+++ src/store/postReducer.ts
@@ -1,4 +1,16 @@
-import {ActionType, GetPostType, GetPostsType, SetPostType, SetPostsType, PostType, StateType} from "../types/types";
+import {
+  ActionType,
+  GetPostType,
+  GetPostsType,
+  SetPostType,
+  SetPostsType,
+  PostType,
+  StateType,
+  UpdatePostType,
+  DeletePostType,
+  CreatePostType,
+  CreateCommentType, SetUpdatedPostType, ClearDeletedPostType, SetCreatedPostType
+} from "../types/types";
 
 export const GET_POSTS = 'GET_POSTS';
 export const SET_POSTS = 'SET_POSTS';
@@ -8,6 +20,11 @@ export const UPDATE_POST = 'UPDATE_POST';
 export const SET_UPDATED_POST = 'SET_UPDATED_POST';
 export const DELETE_POST = 'DELETE_POST';
 export const CLEAR_DELETED_POST = 'CLEAR_DELETED_POST';
+export const CREATE_POST = 'CREATE_POST';
+export const SET_CREATED_POST = 'SET_CREATED_POST';
+export const CREATE_COMMENT = 'CREATE_COMMENT';
+export const SET_CREATED_COMMENT = 'SET_CREATED_COMMENT';
+
 
 const initialState: StateType = {
   posts: null,
@@ -38,3 +55,23 @@ export const getPosts = (): GetPostsType => ({type: GET_POSTS})
 export const setPosts = (posts: Array<PostType>): SetPostsType => ({type: SET_POSTS, posts})
 export const getPost = (id: number): GetPostType => ({type: GET_POST, id})
 export const setPost = (post: PostType): SetPostType => ({type: SET_POST, post})
+export const deletePost = (postId: number): DeletePostType => ({type: DELETE_POST, postId})
+export const clearDeletedPost = (postId: number): ClearDeletedPostType => ({type: CLEAR_DELETED_POST, postId})
+export const createPost = (title: string, body: string): CreatePostType => ({type: CREATE_POST, title, body})
+// Под вопросом: надо ли setCreatedPost вообще? Или можно просто сделать запрос на обновление постов
+export const setCreatedPost = (title: string, body: string): SetCreatedPostType => ({
+  type: SET_CREATED_POST, title, body
+})
+export const updatePost = (postId: number, title: string, body: string): UpdatePostType => ({
+  type: UPDATE_POST, postId, title, body
+})
+export const setUpdatedPost = (postId: number, title: string, body: string): SetUpdatedPostType => ({
+  type: SET_UPDATED_POST, postId, title, body
+})
+export const createComment = (postId: number, body: string): CreateCommentType => ({
+  type: CREATE_COMMENT, postId, body
+})
+// Под вопросом: надо ли setCreatedComment вообще? Или можно просто сделать запрос на обновление комментов
+export const setCreatedComment = (title: string, body: string): SetCreatedPostType => ({
+  type: SET_CREATED_POST, title, body
+})
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,14 @@
-import {GET_POST, GET_POSTS, SET_POST, SET_POSTS} from "../store/postReducer";
+import {
+  CLEAR_DELETED_POST,
+  CREATE_COMMENT,
+  CREATE_POST,
+  DELETE_POST,
+  GET_POST,
+  GET_POSTS, SET_CREATED_COMMENT, SET_CREATED_POST,
+  SET_POST,
+  SET_POSTS, SET_UPDATED_POST,
+  UPDATE_POST
+} from "../store/postReducer";
 
 // PostReducer types
 export type CommentType = {
@@ -23,3 +33,14 @@ export type GetPostsType = {type: typeof GET_POSTS}
 export type SetPostsType = {type: typeof SET_POSTS, posts: Array<PostType>}
 export type GetPostType = {type: typeof GET_POST, id: number}
 export type SetPostType = {type: typeof SET_POST, post: PostType}
+export type UpdatePostType = {type: typeof UPDATE_POST, postId: number, title: string, body: string}
+export type SetUpdatedPostType = {type: typeof SET_UPDATED_POST, postId: number, title: string, body: string}
+export type DeletePostType = {type: typeof DELETE_POST, postId: number}
+export type ClearDeletedPostType = {type: typeof CLEAR_DELETED_POST, postId: number}
+export type CreatePostType = {type: typeof CREATE_POST, title: string, body: string}
+// Под вопросом: надо ли SetCreatedPostType вообще? Или можно просто сделать запрос на обновление постов
+export type SetCreatedPostType = {type: typeof SET_CREATED_POST, title: string, body: string}
+export type CreateCommentType = {type: typeof CREATE_COMMENT, postId: number, body: string}
+// Под вопросом: надо ли SetCreatedCommentType вообще? Или можно просто сделать запрос на обновление комментов
+export type SetCreatedCommentType = {type: typeof SET_CREATED_COMMENT, postId: number, body: string}
+
