3dc22132bc7116b613cc30acb7571269a9041a3d
Danium <Kividanya964@gmail.com>
2020-06-04 16:02:04 +0300

    api is finished

--- src/api/api.ts
+++ src/api/api.ts
@@ -12,5 +12,17 @@ export const API = {
   async getPost(id: number) {
     return await instance.get(`posts/${id}?_embed=comments`)
       .then(response => response.data)
+  },
+  async deletePost(id: number) {
+    return await instance.delete(`posts/${id}`)
+  },
+  async updatePost(id: number, title: string, body: string) {
+    return await instance.put(`posts/${id}`, {title, body})
+  },
+  async createPost(title: string, body: string) {
+    return await instance.post('posts', {title, body})
+  },
+  async createComment(postId: number, body: string) {
+    return await instance.post(`comments`, {postId, body})
   }
 }
