0418b1c87f04162fc7f4cd9d0c396df4b626865b
Danium <Kividanya964@gmail.com>
2020-06-04 14:24:25 +0300

    Comments components structure is finished

--- src/components/PostsPage/Comments/Comment.tsx
+++ src/components/PostsPage/Comments/Comment.tsx
@@ -4,7 +4,8 @@ import {CommentType} from "../../../types/types";
 const Comment: React.FC<CommentType> = ({postId, id, body}) => {
   return (
     <div>
-
+    <div>{id}</div>
+    <div>{body}</div>
     </div>
   )
 }
--- src/components/PostsPage/Comments/Comments.tsx
+++ src/components/PostsPage/Comments/Comments.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import {CommentType} from "../../../types/types";
+import Comment from "./Comment";
 
 type CommentsProps = {
   comments: Array<CommentType> | null
 }
 
 const Comments: React.FC<CommentsProps> = ({comments}) => {
+  const allComments = comments === null ?
+    null :
+    comments.map(comment => <Comment
+      key={comment.id}
+      id={comment.id}
+      postId={comment.postId}
+      body={comment.body}
+    />)
+
   return (
     <div>
-
+      {allComments}
     </div>
   )
 }
