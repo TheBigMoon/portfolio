4dea957fea502a78668cf155059dbae8eb6fbdbd
Danium <Kividanya964@gmail.com>
2020-06-05 13:20:59 +0300

    contents styles is added

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import {Route} from 'react-router-dom';
 import PostsPageContainer from "./components/PostsPage/PostsPageContainer";
 import PostPageContainer from "./components/PostsPage/PostPageContainer";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Header/>
-      <Route path={'/posts'} render={() => <PostsPageContainer/>}/>
-      <Route path={'/post/:postId'} render={() => <PostPageContainer/>}/>
+      <div className={'content'}>
+        <Route path={'/posts'} render={() => <PostsPageContainer/>}/>
+        <Route path={'/post/:postId'} render={() => <PostPageContainer/>}/>
+      </div>
+      <Footer/>
     </div>
   );
 }
