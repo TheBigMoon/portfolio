23e975e113613bb7e8b474b3ee3a79ba6eba041a
Danium <Kividanya964@gmail.com>
2020-06-10 15:29:17 +0300

    Footer component is ready

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import s from './Footer.module.css'
+import telegram from '../../multimedia/telegram.png';
+import phone from '../../multimedia/phone.png';
+import gmail from '../../multimedia/gmail.png';
 
 const Footer = () => {
   return (
     <div className={s.footer}>
-      All rights reserved
+      <img src={telegram} alt=""/>
+      <div>
+        <a target="_blank" rel="noopener noreferrer" href='https://t.me/fist_off'>@Fist_off</a>
+      </div>
+      <img src={gmail} alt=""/>
+      <div>
+        Danium.kulakov@gmail.com
+      </div>
+      <img src={phone} alt=""/>
+      <div>
+        380632786151
+      </div>
     </div>
   )
 }
