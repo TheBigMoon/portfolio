3acf4ae016297b8842b0c4cb8c4f70dd153d58e3
Danium <Kividanya964@gmail.com>
2020-06-11 13:03:53 +0300

    Comment styles is changed

--- src/components/PostsPage/Comments/Comment/Comment.tsx
+++ src/components/PostsPage/Comments/Comment/Comment.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import {CommentType} from "../../../../types/types";
 import s from './Comment.module.css'
+import user from '../../../../multimedia/user.png';
 
-const Comment: React.FC<CommentType> = ({postId, id, body}) => {
+const Comment: React.FC<CommentType> = ({body}) => {
   return (
     <div className={s.comment}>
-      — {body}
+      <div className={s.user}>
+        <img src={user} alt=""/>
+      </div>
+      <div className={s.body}>
+        <p>Name Surname</p>
+        {body}
+      </div>
     </div>
   )
 }
