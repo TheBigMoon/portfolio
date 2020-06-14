a8f11d4272655f064b08c1750065831fef2832d4
Danium <Kividanya964@gmail.com>
2020-06-14 19:43:29 +0300

    all rules is added

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,19 @@ module.exports = {
       '@typescript-eslint'
   ],
   'rules': {
-
+    'linebreak-style': ['error', 'windows'],
+    'comma-dangle': ['error', 'never'],
+    'arrow-parens': ['error', 'always'],
+    'import/extensions': ['error', 'never'],
+    'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'eol-last': ['error', 'never'],
+    'no-shadow': 'off',
+    'react/jsx-curly-brace-presence': 'off',
+    'no-unused-vars': 'off',
+    'import/prefer-default-export': 'off',
+    'no-return-await': 'off',
+    'react/prop-types': 'off',
+    'import/no-unresolved': 'off',
   }
 };
